daca7018bf0986858242454194703dd70c1fc7e4
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-02 15:50:46 +1100

    Modify 'index.js' to add more inquirer prompts for README sections

--- index.js
+++ index.js
@@ -39,12 +39,50 @@ const promptUser = () => {
                     name: 'Questions',
                 }
             ]
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Type your project description:",
+            when: (answers) => answers.content.includes('Description')
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: "Type your project installation instructions:",
+            when: (answers) => answers.content.includes('Installation')
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: "Type your project usage information:",
+            when: (answers) => answers.content.includes('Usage')
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: "Select your project license:",
+            choices: ['Apache 2.0 License', 'GNU GPLv3', 'ISC', 'MIT'],
+            when: (answers) => answers.content.includes('License')
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: "Type your project contribution guidelines:",
+            when: (answers) => answers.content.includes('Contributing')
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: "Type your project test instructions:",
+            when: (answers) => answers.content.includes('Tests')
         }
     ]);
 };
 
 // const generateREADME = (answers) =>
 // `# ${answers.title}
+
 // `
 // ;
 
