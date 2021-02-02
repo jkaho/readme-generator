5ce7b90f654bf750766208422766504fdca6897a
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-02 15:29:50 +1100

    Modify 'index.js' to add inquirer prompts for project title and README contents

--- index.js
+++ index.js
@@ -2,3 +2,58 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?'            
+        },
+        {
+            type: 'checkbox',
+            name: 'content',
+            message: 'Select the sections you would like to include in your README file:',
+            choices: [
+                {
+                    name: 'Description',
+                },
+                {
+                    name: 'Table of Contents',
+                },
+                {
+                    name: 'Installation',
+                },
+                {
+                    name: 'Usage',
+                },
+                {
+                    name: 'License',
+                },
+                {
+                    name: 'Contributing',
+                },
+                {
+                    name: 'Tests',
+                },
+                {
+                    name: 'Questions',
+                }
+            ]
+        }
+    ]);
+};
+
+// const generateREADME = (answers) =>
+// `# ${answers.title}
+// `
+// ;
+
+const init = () => {
+    promptUser()
+    .then((answers) =>
+    {
+        console.log(answers.content);
+    })
+}
+
+init();
