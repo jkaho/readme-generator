3baca6d389e5483fc16974cdafd735e438bf8949
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-02 16:05:01 +1100

    Modify 'generateMarkdown.js' to create function to render license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,15 @@
 // returns license badge
-function renderLicenseBadge(license){}
+function renderLicenseBadge(license){
+    if (license === 'Apache 2.0 License') {
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    } else if (license === 'GNU GPLv3') {
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    } else if (license === 'ISC') {
+        return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+    } else {
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    }
+}
 
 // returns license link
 function renderLicenseLink(license){}
